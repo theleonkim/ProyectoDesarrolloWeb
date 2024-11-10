c18d8b2295270a3209f41d5ac1dec0e9726c95a3
Kimberly Leon <Kimberly Leon@LAPTOP-9OPLIQPF>
2024-11-10 17:03:27 -0600

    Avance #2 Kimberly

---
+++ ProyectoRE/src/main/java/com/ProyectoRE/ProyectoReApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoRE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoReApplication.class, args);
+	}
+
+}
---
+++ ProyectoRE/src/test/java/com/ProyectoRE/ProyectoReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoRE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
