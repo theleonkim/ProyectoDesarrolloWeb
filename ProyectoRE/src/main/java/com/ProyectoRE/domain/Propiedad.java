0e11fe69d66121d7179344962cf11352015d437f
PriscilaRodriguez26 <prodriguez80843@ufide.ac.cr>
2024-11-11 03:10:02 -0600

    Se agrega clase propiedad

---
+++ ProyectoRE/src/main/java/com/ProyectoRE/domain/Propiedad.java
@@ -0,0 +1,68 @@
+
+package com.ProyectoRE.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+
+
+@Data
+@Entity
+@Table(name="propiedad")
+public class Propiedad implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name="id_propiedad")
+    private int idPropiedad;
+    private int idTipoPropiedad;
+    private int idServicioPremium;
+    private String nombre;
+    private String descripcion;
+    private int numHabitaciones;
+    private int numBanos;
+    private int numAreasSociables;
+    private float precio;
+    private int mtsLote;
+    private int mtsConstruccion;
+    private boolean ameneties;
+    private String urlUbicacion;
+    private String urlImgExterior1;
+    private String urlImgExterior2;
+    private String urlImgInterior1;
+    private String urlImgInterior2;
+    private boolean estado;
+
+    public Propiedad() {
+    }
+
+    public Propiedad(int idTipoPropiedad, int idServicioPremium, String nombre, String descripcion, int numHabitaciones, int numBanos, int numAreasSociables, float precio, int mtsLote, int mtsConstruccion, boolean ameneties, String urlUbicacion, String urlImgExterior1, String urlImgExterior2, String urlImgInterior1, String urlImgInterior2, boolean estado) {
+        this.idTipoPropiedad = idTipoPropiedad;
+        this.idServicioPremium = idServicioPremium;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.numHabitaciones = numHabitaciones;
+        this.numBanos = numBanos;
+        this.numAreasSociables = numAreasSociables;
+        this.precio = precio;
+        this.mtsLote = mtsLote;
+        this.mtsConstruccion = mtsConstruccion;
+        this.ameneties = ameneties;
+        this.urlUbicacion = urlUbicacion;
+        this.urlImgExterior1 = urlImgExterior1;
+        this.urlImgExterior2 = urlImgExterior2;
+        this.urlImgInterior1 = urlImgInterior1;
+        this.urlImgInterior2 = urlImgInterior2;
+        this.estado = estado;
+    }
+    
+    
+}
