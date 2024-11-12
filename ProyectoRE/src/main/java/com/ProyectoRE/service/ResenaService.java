e3db66d76fd73760c31724fed42feafabb133efe
Kimberly Leon <Kimberly Leon@LAPTOP-9OPLIQPF>
2024-11-12 11:02:13 -0600

    Avance #2 Kimberly | Corrección de errores

--- ProyectoRE/src/main/java/com/ProyectoRE/service/ResenaService.java
+++ ProyectoRE/src/main/java/com/ProyectoRE/service/ResenaService.java
@@ -5,16 +5,16 @@ import com.ProyectoRE.domain.Resenas;
 import java.util.List;
 
 
+
 public interface ResenaService {
-    
-   
-    public List<Resenas> getResenas(boolean estado);
-    
-   
-    public Resenas getResena(Resenas resena);
-    
-  
-    public void save(Resenas resena);
-    
-    
+
+    // Método para obtener una lista de reseñas basadas en el estado
+    List<Resenas> getResenas(boolean estado);
+
+    // Método para obtener una reseña específica
+    Resenas getResena(Resenas resena);
+
+    // Método para guardar una reseña
+    void save(Resenas resena);
 }
+
--- ProyectoRE/src/main/java/com/ProyectoRE/service/impl/ResenaServiceImpl.java
+++ ProyectoRE/src/main/java/com/ProyectoRE/service/impl/ResenaServiceImpl.java
@@ -1,9 +1,9 @@
-
 package com.ProyectoRE.service.impl;
 
 import com.ProyectoRE.dao.ResenasDao;
 import com.ProyectoRE.domain.Resenas;
 import com.ProyectoRE.service.ResenaService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,17 +14,21 @@ public class ResenaServiceImpl implements ResenaService {
     @Autowired
     private ResenasDao resenasDao;
     
+    @Override
+    @Transactional(readOnly = true)
+    public List<Resenas> getResenas(boolean estado) {
+        return resenasDao.findByEstado(estado);
+    }
 
     @Override
     @Transactional(readOnly = true)
-    public Resenas getResenas(Resenas resenas) {
-        return resenasDao.findById(resenas.getIdResena()).orElse(null);
+    public Resenas getResena(Resenas resena) {
+        return resenasDao.findById(resena.getIdResena()).orElse(null);
     }
 
     @Override
     @Transactional
-    public void save(Resenas resenas) {
-        resenasDao.save(resenas);
+    public void save(Resenas resena) {
+        resenasDao.save(resena);
     }
-    
 }
