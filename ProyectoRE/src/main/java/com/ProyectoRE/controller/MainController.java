0569c16110b26eaf0a83e0f092dbcb8d6d8ff26f
Kimberly Leon <Kimberly Leon@LAPTOP-9OPLIQPF>
2024-11-20 10:39:17 -0600

    Proyecto Real estate

---
+++ ProyectoRE/src/main/java/com/ProyectoRE/controller/MainController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+
+/**
+ *
+ * @author Kimberly Leon
+ */
+package com.ProyectoRE.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public String home() {
+        return "index"; // Redirige a index.html
+    }
+
+    @GetMapping("/luxuryproperties")
+    public String luxuryProperties() {
+        return "luxuryproperties"; // Redirige a luxuryproperties.html
+    }
+
+    @GetMapping("/compare")
+    public String compareProperties() {
+        return "compare"; // Redirige a compare.html
+    }
+
+    @GetMapping("/map")
+    public String map() {
+        return "map"; // Redirige a map.html
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact"; // Redirige a contact.html
+    }
+
+    @GetMapping("/favorites")
+    public String favorites() {
+        return "favorites"; // Redirige a favorites.html
+    }
+
+    @GetMapping("/reviews")
+    public String reviews() {
+        return "reviews"; // Redirige a reviews.html
+    }
+}
