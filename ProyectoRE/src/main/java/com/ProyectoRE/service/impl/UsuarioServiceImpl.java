c8541a8f46079b3e52ac02b49405c7a5f832dd9f
PriscilaRodriguez26 <prodriguez80843@ufide.ac.cr>
2024-12-17 23:56:10 -0600

    Ajustes de compilado

--- ProyectoRE/src/main/java/com/ProyectoRE/service/impl/UsuarioServiceImpl.java
+++ ProyectoRE/src/main/java/com/ProyectoRE/service/impl/UsuarioServiceImpl.java
@@ -7,7 +7,6 @@ import com.ProyectoRE.service.UsuarioService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
